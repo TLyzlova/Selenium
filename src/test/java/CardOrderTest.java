1a8a88d9b0bd3252488787fe0d9be9f2566a4730
Lyzlova Tatiana <yatanita@gmail.com>
2021-09-06 21:24:32 +0300

    Driver runs but test failed

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -15,7 +15,7 @@ class CardOrderTest {
    @BeforeAll
 
     static void setUpAll() {
-       System.setProperty("webdriver.chrome.driver", "./driver.win.chromedriver");
+       System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
    }
 
    @BeforeEach
